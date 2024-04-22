77c1323fda0325154c0403969c31a2ec9a39a3f9
fabric0de <solee3013@gmail.com>
2024-04-22 23:45:23 +0900

    firebase 세팅 및 로그인&회원가입 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import { app } from "./firebaseApp";
+import { getAuth } from "firebase/auth";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Router from "./components/Router";
 
 function App() {
+  const auth = getAuth(app);
+
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
   return (
     <>
-      <Router />
+      <ToastContainer />
+      <Router isAuthenticated={isAuthenticated} />
     </>
   );
 }
---
+++ src/components/SignupForm.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+
+import { Link } from "react-router-dom";
+import { app } from "firebaseApp";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
+
+export default function SignupForm() {
+  const [error, setError] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordConfirm, setPasswordConfirm] = useState<string>("");
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth(app);
+      await createUserWithEmailAndPassword(auth, email, password);
+
+      toast.success("회원가입이 완료되었습니다.");
+    } catch (error: any) {
+      console.error(error);
+      toast.error(error?.code);
+    }
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      setEmail(value);
+      const validRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+      if (!value?.match(validRegex)) {
+        setError("이메일 형식이 올바르지 않습니다.");
+      } else {
+        setError("");
+      }
+    }
+
+    if (name === "password") {
+      setPassword(value);
+
+      if (value?.length < 8) {
+        setError("비밀번호는 8자 이상이어야 합니다.");
+      } else if (passwordConfirm?.length > 0 && value !== passwordConfirm) {
+        setError("비밀번호와 비밀번호 확인 값이 일치하지 않습니다.");
+      } else {
+        setError("");
+      }
+    }
+
+    if (name === "password__confirm") {
+      setPasswordConfirm(value);
+
+      if (password !== value) {
+        setError("비밀번호가 일치하지 않습니다.");
+      } else {
+        setError("");
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="form form--lg">
+      <h1 className="form__title">로그인</h1>
+      <div className="form__block">
+        <label htmlFor="email">이메일</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          onChange={onChange}
+        />
+      </div>
+      <div className="form__block">
+        <label htmlFor="password">비밀번호</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          onChange={onChange}
+        />
+      </div>
+      <div className="form__block">
+        <label htmlFor="password__confirm">비밀번호 확인</label>
+        <input
+          type="password"
+          id="password__confirm"
+          name="password__confirm"
+          required
+          onChange={onChange}
+        />
+      </div>
+      {error && error?.length > 0 && (
+        <div className="form__block">
+          <div className="form__error">{error}</div>
+        </div>
+      )}
+      <div className="form__block">
+        계정이 이미 있으신가요? &nbsp;
+        <Link to="/login" className="form__link">
+          로그인
+        </Link>
+      </div>
+      <div className="form__block">
+        <input
+          type="submit"
+          value="회원가입"
+          className="form__btn--submit"
+          disabled={error?.length > 0}
+        />
+      </div>
+    </form>
+  );
+}
