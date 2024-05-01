d12e1f9d72be7f56c64745ff02bc7152edfc9401
fabric0de <solee3013@gmail.com>
2024-05-01 16:51:31 +0900

    댓글 기능 추가

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -18,7 +18,12 @@ interface PostListProps {
   defaultTab?: TabType | CategoryType;
 }
 
-type TabType = "all" | "my";
+export interface CommentsInterface {
+  content: string;
+  uid: string;
+  email: string;
+  createdAt: string;
+}
 
 export interface PostProps {
   id?: string;
@@ -30,7 +35,10 @@ export interface PostProps {
   updatedAt?: string;
   uid: string;
   category?: CategoryType;
+  comments?: CommentsInterface[];
 }
+
+type TabType = "all" | "my";
 
 export type CategoryType = "Frotend" | "Backend" | "DevOps" | "Design" | "Etc";
 export const CATEGORIES: CategoryType[] = [
